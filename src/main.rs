f0c2dda0f720bb95c928e7f811ee8a9df0d31e58
mio <mio@lazym.io>
2025-04-25 22:59:36 +0800

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,50 @@
+use alloy::providers::{Provider, ProviderBuilder};
+use chrono::{DateTime, Utc};
+use clap::Parser;
+use color_eyre::Result;
+use incr_stats::incr::Stats;
+use tokio_stream::StreamExt;
+
+#[derive(Parser)]
+struct RpcArgs {
+    #[arg(short, long)]
+    pub rpc: String,
+}
+
+async fn measure(args: RpcArgs) -> Result<()> {
+    let rpc = ProviderBuilder::new().connect(&args.rpc).await?;
+
+    let mut sub = rpc.subscribe_full_blocks().into_stream().await?;
+    let mut s = Stats::new();
+
+    println!("Start to listen blocks...");
+    while let Some(block) = sub.next().await {
+        let now = Utc::now();
+        let block = block?;
+        let ts = DateTime::from_timestamp(block.header.timestamp as i64, 0).unwrap();
+        let diff = (now - ts).num_milliseconds();
+        s.update(diff as f64)?;
+        println!(
+            "Block {} arrived, current: {:.02} ms, avg: {:.02} ms, std: {:.02}, max: {:.02} ms, min: {:.02} ms",
+            block.header.number,
+            diff,
+            s.mean().unwrap(),
+            s.sample_standard_deviation().unwrap_or_default(),
+            s.max().unwrap(),
+            s.min().unwrap()
+        );
+    }
+
+    Ok(())
+}
+
+fn main() {
+    color_eyre::install().unwrap();
+    let args = RpcArgs::parse();
+    tokio::runtime::Builder::new_multi_thread()
+        .enable_all()
+        .build()
+        .unwrap()
+        .block_on(measure(args))
+        .expect("??");
+}
